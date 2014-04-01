b8ef4cd3b530252c0049947473f929827d737b76
Hugo Duncan <hugo@hugoduncan.org>
2014-04-01 18:50:58 -0400

    Add travis configuration

--- profiles.clj
+++ profiles.clj
@@ -5,10 +5,13 @@
                  [ch.qos.logback/logback-classic "1.0.9"]]
   :plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]
             [com.palletops/lein-pallet-crate "0.1.0"]
-            [lein-set-version "0.3.0"]
+            [lein-pallet-release "0.1.3"]
             [lein-resource "0.3.2"]
             [codox/codox.leiningen "0.6.4"]
             [lein-marginalia "0.7.1"]]
+  :pallet-release
+  {:url "https://pbors:${GH_TOKEN}@github.com/pallet/mongodb-crate.git"
+   :branch "master"}
   :aliases {"live-test-up"
             ["pallet" "up" "--phases" "install,configure,test"]
             "live-test-down" ["pallet" "down"]
@@ -22,6 +25,7 @@
   :test-selectors {:default (complement :live-test)
                    :live-test :live-test
                    :all (constantly true)}}
+ :no-checkouts {:checkout-deps-shares ^{:replace true} []}
  :doc {:dependencies [[com.palletops/pallet-codox "0.1.0"]]
        :plugins [[codox/codox.leiningen "0.6.4"]
                  [lein-marginalia "0.7.1"]]
@@ -35,6 +39,5 @@
                  "doc" ["do" "codox," "marg"]}
        }
  :release
- {:plugins [[lein-set-version "0.3.0"]]
-  :set-version
+ {:set-version
   {:updates [{:path "README.md" :no-snapshot true}]}}}
